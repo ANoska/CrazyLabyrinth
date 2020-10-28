2f661d211c4a35e41102eceb0786472ce5465b8a
Austin Noska <noska.austin@gmail.com>
2020-10-28 14:13:26 -0400

    Add camera script that follows the player

---
+++ Assets/_Scripts/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = player.transform.position + offset;
+        transform.position = desiredPosition;
+    }
+}
