fecd59546bc6cf252bfe910ee14dad791b9b9264
Jake Eckert <BE891702@wcupa.edu>
2020-10-29 19:37:57 -0400

    Simple scripts written for the holes, Hole colliders resized for better game behavior.

--- Assets/_Scripts/BadHoles.cs
+++ Assets/_Scripts/BadHoles.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoodHoles : MonoBehaviour
+public class BadHoles : MonoBehaviour
 {
     void OnTriggerEnter(Collider coll) {
-        if(coll.tag == "Player") {
+        if (coll.tag == "Player") {
             Destroy(coll.gameObject);
         }
     }
