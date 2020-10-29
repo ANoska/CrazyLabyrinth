2a17ba5fb8b082026689c3a6d51999e6acc5ab5d
Jake Eckert <BE891702@wcupa.edu>
2020-10-28 21:46:40 -0400

    Script created for GoodHoles, not working yet...

---
+++ Assets/_Scripts/GoodHoles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodHoles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collision coll) {
+        //Find out what hit the hole
+        //GameObject collideWith = coll.gameObject;
+        if(coll.gameObject.tag == "Player") {
+            //NEXT LEVEL
+            Destroy(gameObject);
+        }
+
+    }
+}
