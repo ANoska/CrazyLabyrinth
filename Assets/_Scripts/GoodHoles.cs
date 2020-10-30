5d8b6bf268eba84f396401fe637037aca80dc61f
Jake Eckert <BE891702@wcupa.edu>
2020-10-29 20:51:43 -0400

    Ball actually falls into the holes now. It's cool!

--- Assets/_Scripts/GoodHoles.cs
+++ Assets/_Scripts/GoodHoles.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class GoodHoles : MonoBehaviour
 {
+    public GameObject cam;
+    public GameObject player;
+    public Rigidbody playerBody;
+
+    float timeForLerp = Time.time;
+    void Start() {
+        cam = GameObject.Find("Main Camera");
+        player = GameObject.FindWithTag("Player");
+        playerBody = player.GetComponent<Rigidbody>();
+    }
+
     void OnTriggerEnter(Collider coll) {
-        if(coll.tag == "Player") {
-            Destroy(coll.gameObject);
+        if (coll.tag == "Player") {
+
+            //IMPORTANT: these need to be reset to true when the player respawns
+            //fall through floor:
+            coll.GetComponent<Collider>().enabled = false;
+            //stop follow cam:
+            cam.GetComponent<CameraBehaviour>().enabled = false;
+
+            //move player to center of hole:
+            player.transform.position = this.transform.position;
+
+            //using lerp would make the fall-in motion much smoother, but requires time calculation:
+            //player.transform.position = Vector3.Lerp(transform.position, this.gameObject.transform.position, .05f);
+
+
+            //stop motion in all horizontal axes:
+            playerBody.constraints = RigidbodyConstraints.FreezePositionX;
+            playerBody.constraints = RigidbodyConstraints.FreezePositionZ;
         }
     }
 }
